0f30bf274a67ff2d90b04ad4f958437ccdcf7e91
cedricalexander <54778098+cedricalexander@users.noreply.github.com>
2019-11-25 16:26:19 +0100

    re-work
    i reshaped the files again

--- app.py
+++ app.py
@@ -1,69 +1,43 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "#Here's a quick example\n",
-    "# ALWAYS STOP KERNEL WITH STOP BUTTON BEFORE CLOSING NOTEBOOK BECAUSE IT RUNS FOREVER\n",
-    "\n",
-    "import dash\n",
-    "import dash_core_components as dcc\n",
-    "import dash_html_components as html\n",
-    "\n",
-    "app = dash.Dash(__name__)\n",
-    "server = app.server\n",
-    "app.css.append_css({\"external_url\": \"https://codepen.io/chriddyp/pen/bWLwgP.css\"})\n",
-    "\n",
-    "app.layout = html.Div(children=[\n",
-    "    html.H1(children='Hello Dash'),\n",
-    "\n",
-    "    html.Div(children='''\n",
-    "        Dash: A web application framework for Python.\n",
-    "    '''),\n",
-    "\n",
-    "    dcc.Graph(\n",
-    "        id='example-graph',\n",
-    "        figure={\n",
-    "            'data': [\n",
-    "                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'line', 'name': 'SF'},\n",
-    "                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'line', 'name': u'Montréal'},\n",
-    "            ],\n",
-    "            'layout': {\n",
-    "                'title': 'Dash Data Visualization'\n",
-    "            }\n",
-    "        }\n",
-    "    )\n",
-    "])\n",
-    "\n",
-    "if __name__ == '__main__':\n",
-    "    app.run_server(debug=False)\n",
-    "\n",
-    "# ALWAYS STOP KERNEL WITH STOP BUTTON BEFORE CLOSING NOTEBOOK BECAUSE IT RUNS FOREVER"
-   ]
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.7.3"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 2
-}
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#Here's a quick example
+# ALWAYS STOP KERNEL WITH STOP BUTTON BEFORE CLOSING NOTEBOOK BECAUSE IT RUNS FOREVER
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+app = dash.Dash(__name__)
+server = app.server
+app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
+
+app.layout = html.Div(children=[
+    html.H1(children='Hello Dash'),
+
+    html.Div(children='''
+        Dash: A web application framework for Python.
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure={
+            'data': [
+                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'line', 'name': 'SF'},
+                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'line', 'name': u'Montréal'},
+            ],
+            'layout': {
+                'title': 'Dash Data Visualization'
+            }
+        }
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=False)
+
+# ALWAYS STOP KERNEL WITH STOP BUTTON BEFORE CLOSING NOTEBOOK BECAUSE IT RUNS FOREVER
+
